a9f10e242cc57641e5ea0f03c056dd36c9e49bd3
norimoji <zimmerzing@gmail.com>
2017-08-02 17:47:13 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '\dist'));
+app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
 console.log("Server up on port 8080");
 
