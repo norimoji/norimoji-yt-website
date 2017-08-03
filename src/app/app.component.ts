495f1f7dbfe3676ec7c91ccd8d9cc54a883fe7e9
norimoji <zimmerzing@gmail.com>
2017-08-03 13:42:12 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, enableProdMode } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
 import * as $ from 'jquery';
 
@@ -13,21 +13,15 @@ const API_TOKEN = 'AIzaSyDRey5YC2tvPZQ7ikD9F8Q5qHJYO65_PVM';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent implements OnInit {
+export class AppComponent{
   title = 'app';
   isDarkTheme = false;
-  path = '../assets/youtubescript.js';
   myData: Array<any>;
 
   constructor(private _http: Http) {
-    this._http.get('`${FUNCTION_URL}?part=snippet&maxResults=10&playlistId=UUuYp_6i2jGf6ZvWo-Ql4wZA&key=${API_TOKEN}')
+    this._http.get('${FUNCTION_URL}?part=snippet&maxResults=10&playlistId=UUuYp_6i2jGf6ZvWo-Ql4wZA&key=${API_TOKEN}')
         .map(response => response.json())
         .subscribe(res => this.myData = res);
 }
-       ngOnInit():any{  
-        $(document).ready(function(){
-
-        })
-    }
 }
 
